00d911a21bf7fcf0ca811aa21764846cfb064531
halaxhenry <halaxhenry@gmail.com>
2022-03-11 12:35:09 +0900

    Initial Commit

---
+++ VideoPlayerDemo/VideoPlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerDemoApp.swift
+//  VideoPlayerDemo
+//
+//  Created by Seungchul Ha on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
